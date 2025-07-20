41da7a624b6081a3c33c97b6a1b61def48a75f3f
AgnikMukherjee <agnikmukherjee78@gmail.com>
2025-07-20 17:46:00 +0530

    layout, sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,6 +20,7 @@ const App = () => {
         <Route path="/ai" element={<Layout />}>
           <Route index element={<Dashboard />} />
           <Route path="write-article" element={<WriteArticle />} />
+          <Route path="blog-titles" element={<BlogTitles />} />
           <Route path="generate-images" element={<GeneratedImages />} />
           <Route path="remove-background" element={<RemoveBackground />} />
           <Route path="remove-object" element={<RemoveObject />} />
