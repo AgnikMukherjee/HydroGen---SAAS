a447a08e6d5ed4642e785f81476c4ed9bbad892c
AgnikMukherjee <agnikmukherjee78@gmail.com>
2025-08-01 22:37:39 +0530

    ai conrollers

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,6 +1,7 @@
 import OpenAI from "openai";
 import sql from "../configs/db.js";
 import { clerkClient } from "@clerk/express";
+import { response } from "express";
 
 
 const AI = new OpenAI({
@@ -18,7 +19,7 @@ export const generateArticle = async (req, res) => {
         const plan = req.plan;
         const free_usage = req.free_usage;
 
-        if (plan != 'premium' && free_usage >= 20) {
+        if (plan != 'premium' && free_usage >= 10) {
             return res.json({ success: false, message: 'You have reached your free limit. Upgrade to continue.' });
         }
 
@@ -37,14 +38,88 @@ export const generateArticle = async (req, res) => {
         const content = response.choices[0].message.content;
         await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${prompt}, ${content}, 'article')`;
 
-        if(plan != 'premium') {
+        if (plan != 'premium') {
             await clerkClient.users.updateUserMetadata(userId, {
                 privateMetadata: {
                     free_usage: free_usage + 1
                 }
             });
         }
-        res.json({ success: true, message: 'Article generated successfully.', content });
+        res.json({ success: true, content });
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+}
+
+export const generateBlogTitle = async (req, res) => {
+    try {
+        const { userId } = req.auth();
+        const { prompt } = req.body;
+        const plan = req.plan;
+        const free_usage = req.free_usage;
+
+        if (plan != 'premium' && free_usage >= 10) {
+            return res.json({ success: false, message: 'You have reached your free limit. Upgrade to continue.' });
+        }
+
+        const response = await AI.chat.completions.create({
+            model: "gemini-2.0-flash",
+            messages: [
+                {
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+            temperature: 0.7,
+            max_tokens: 100
+        });
+
+        const content = response.choices[0].message.content;
+        await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${prompt}, ${content}, 'blog-title')`;
+
+        if (plan != 'premium') {
+            await clerkClient.users.updateUserMetadata(userId, {
+                privateMetadata: {
+                    free_usage: free_usage + 1
+                }
+            });
+        }
+        res.json({ success: true, content });
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+}
+
+export const generateImage = async (req, res) => {
+    try {
+        const { userId } = req.auth();
+        const { prompt, publish } = req.body;
+        const plan = req.plan;
+
+        if (plan != 'premium') {
+            return res.json({ success: false, message: 'This feature is only available in premium plan' });
+        }
+
+        const formData = new FormData()
+        formData.append('prompt', prompt);
+        await axios.post("https://clipdrop-api.co/text-to-image/v1", formData, {
+            Headers: {'x-api-key': YOUR_API_KEY},
+            responseType: 'arraybuffer'
+        })
+
+        const content = response.choices[0].message.content;
+        await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${prompt}, ${content}, 'blog-title')`;
+
+        if (plan != 'premium') {
+            await clerkClient.users.updateUserMetadata(userId, {
+                privateMetadata: {
+                    free_usage: free_usage + 1
+                }
+            });
+        }
+        res.json({ success: true, content });
     } catch (error) {
         console.log(error.message);
         res.json({ success: false, message: error.message });
