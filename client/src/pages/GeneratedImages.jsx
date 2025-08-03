27672c60df16ca176099de9f50e8282e47a61147
AgnikMukherjee <agnikmukherjee78@gmail.com>
2025-08-03 22:07:20 +0530

    connected gen img

--- client/src/pages/GeneratedImages.jsx
+++ client/src/pages/GeneratedImages.jsx
@@ -1,18 +1,45 @@
 import { Image, Sparkles } from 'lucide-react'
 import React, { useState } from 'react'
+import axios from 'axios'
+import { useAuth } from '@clerk/clerk-react';
+import toast from 'react-hot-toast';
+
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+
 
 const GeneratedImages = () => {
-  const imageStyles = ['Realistic', 'Anime style', 'Cartoon style', 'Abstract style', 'Vintage style', 'Modern style', 'Fantasy style', 'Minimalist style', 'Cyberpunk style','Pop art style', '3D style', 'Black and white style', 'Graffiti style',, 'Sketch style', 'Watercolor style', 'Oil painting style',, 'Ghibli Style' ]
-    
-      const [selectedStyle, setSelectedStyle] = useState('Realistic')
-      const [input, setInput] = useState('')
-      const [publish, setPublish] = useState(false)
-    
-      const onSubmitHandler = async (e) => {
-        e.preventDefault()
-        console.log(input, selectedStyle)
+  const imageStyles = ['Realistic', 'Anime style', 'Cartoon style', 'Abstract style', 'Vintage style', 'Modern style', 'Fantasy style', 'Minimalist style', 'Cyberpunk style', 'Pop art style', '3D style', 'Black and white style', 'Graffiti style', , 'Sketch style', 'Watercolor style', 'Oil painting style', , 'Ghibli Style']
+
+  const [selectedStyle, setSelectedStyle] = useState('Realistic')
+  const [input, setInput] = useState('')
+  const [publish, setPublish] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [content, setContent] = useState('')
+
+  const { getToken } = useAuth();
+  const onSubmitHandler = async (e) => {
+    e.preventDefault()
+     try {
+      setLoading(true)
+      const prompt = `Generate an image of ${input} in the style ${selectedStyle}`
+
+      const { data } = await axios.post('/api/ai/generate-image', { prompt, publish }, {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`
+        }
+      })
+
+      if (data.success) {
+        setContent(data.content)
+      } else {
+        toast.error(data.message)
       }
-      
+    } catch (error) {
+      toast.error(error.message)
+    }
+    setLoading(false)
+  }
+
   return (
     <div className='h-full overflow-y-scroll p-6 flex items-start flex-row gap-4 text-slate-700'>
       {/* col 1 */}
@@ -23,7 +50,7 @@ const GeneratedImages = () => {
         </div>
 
         <p className='mt-6 text-sm font-medium'>Describe your image</p>
-        
+
         <textarea onChange={(e) => setInput(e.target.value)} value={input} rows={4} placeholder='Describe what do you want in your image' required className='w-full p-2 px-2 mt-2 outline-none border border-gray-300 rounded-md focus:ring-2 focus:ring-green-500' />
 
         <p className='mt-4 text-sm font-medium'>Style</p>
@@ -41,7 +68,7 @@ const GeneratedImages = () => {
         <div className='flex items-center gap-2 mt-6'>
           <label className="relative cursor-pointer">
             <input type="checkbox" checked={publish} onChange={(e) => setPublish(e.target.checked)} className="sr-only peer" />
-            
+
             <div className='w-9 h-5 bg-slate-300 rounded-full peer-checked:bg-green-500 transition'></div>
 
             <span className='absolute left-1 top-1 w-3 h-3 bg-white rounded-full peer-checked:translate-x-4 transition '></span>
@@ -50,9 +77,10 @@ const GeneratedImages = () => {
         </div>
 
         <br />
-        <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00AD25] to-[#04FF50] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
-          <Image className='w-5' />
-          Generate Title
+        <button disabled={loading} className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00AD25] to-[#04FF50] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+          {loading ? <span className='w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin'></span>
+            : <Image className='w-5' />}
+          Generate Image
         </button>
       </form>
       {/* col 2 */}
@@ -61,12 +89,20 @@ const GeneratedImages = () => {
           <Image className='w-5 h-5 text-[#00AD25]' />
           <h1 className='text-xl font-semibold'>image Preview</h1>
         </div>
-        <div className='flex-1 flex justify-center items-center'>
+
+        {!content ? (
+          <div className='flex-1 flex justify-center items-center'>
           <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
             <Image className='w-9 h-9' />
             <p>Enter a topic and click "Generate Image" to get started</p>
           </div>
         </div>
+        ) : (
+          <div className='mt-3 h-full'>
+            <img src={content} alt="generated image" className='w-full h-full'/>
+          </div>
+        )}
+        
       </div>
     </div>
   )
