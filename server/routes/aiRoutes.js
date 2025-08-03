5e0c7d58f9e5cd45472f7f3a476652f50f954ffa
AgnikMukherjee <agnikmukherjee78@gmail.com>
2025-08-03 20:02:58 +0530

    fixed spelling mistake

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -11,10 +11,10 @@ aiRouter.post('/generate-blog-title', auth, generateBlogTitle);
 
 aiRouter.post('/generate-image', auth, generateImage);
 
-aiRouter.post('/remove-image-background',upload,single('image'), auth, removeImageBackground);
+aiRouter.post('/remove-image-background',upload.single('image'), auth, removeImageBackground);
 
-aiRouter.post('/remove-image-object',upload,single('image'), auth, removeImageObject);
+aiRouter.post('/remove-image-object',upload.single('image'), auth, removeImageObject);
 
-aiRouter.post('/resume-review',upload,single('resume'), auth, reviewResume);
+aiRouter.post('/resume-review',upload.single('resume'), auth, reviewResume);
 
 export default aiRouter;
