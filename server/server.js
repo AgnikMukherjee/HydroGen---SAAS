17df69c7227809940d80abb8be0816f4e2aeb239
AgnikMukherjee <agnikmukherjee78@gmail.com>
2025-08-04 01:24:23 +0530

    updated origin

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ const app = express();
 await connectCloudinary();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({
+  origin: ['http://localhost:5173', 'http://localhost:5174', process.env.CLIENT_URL],
+  credentials: true
+}));
 
 app.use(clerkMiddleware({
     secretKey: process.env.CLERK_SECRET_KEY,
